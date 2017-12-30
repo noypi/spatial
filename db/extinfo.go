9926c446d01b64c68a4633f1e74e01c7b1396cab
noypi <adrian.migraso@gmail.com>
2017-12-31 07:22:48 +0800

    add extinfo tests

--- db/extinfo.go
+++ db/extinfo.go
@@ -10,7 +10,8 @@ func (this *Spatial1D) SetExtInfo(category uint8, id []byte, v interface{}) erro
 		return err
 	}
 
-	buf := bytes.NewBuffer([]byte{cPrefixExtInfo})
+	buf := new(bytes.Buffer)
+	buf.WriteByte(cPrefixExtInfo)
 	buf.Write(id)
 	buf.WriteByte(category)
 
@@ -23,10 +24,36 @@ func (this *Spatial1D) SetExtInfo(category uint8, id []byte, v interface{}) erro
 	return wrtr.ExecuteBatch(batch)
 }
 
+func (this *Spatial1D) GetExtInfo(category uint8, id []byte) (v interface{}, err error) {
+	rdr, err := this.extinfo.Reader()
+	if nil != err {
+		return
+	}
+	buf := new(bytes.Buffer)
+	buf.WriteByte(cPrefixExtInfo)
+	buf.Write(id)
+	buf.WriteByte(category)
+
+	bb, err := rdr.Get(buf.Bytes())
+	if nil != err {
+		return
+	}
+
+	return deserializeRaw(bb)
+}
+
 func (this *Spatial2D) SetExtInfo(category uint8, id []byte, v interface{}) error {
 	return this.x.SetExtInfo(category, id, v)
 }
 
+func (this *Spatial2D) GetExtInfo(category uint8, id []byte) (v interface{}, err error) {
+	return this.x.GetExtInfo(category, id)
+}
+
 func (this *Spatial3D) SetExtInfo(category uint8, id []byte, v interface{}) error {
 	return this.xy.SetExtInfo(category, id, v)
+}
+
+func (this *Spatial3D) GetExtInfo(category uint8, id []byte) (v interface{}, err error) {
+	return this.xy.GetExtInfo(category, id)
 }
